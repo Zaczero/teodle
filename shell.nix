294cc8b4af989ec9f50da776bd27e196987f9154
Kamil Monicz <kamil@monicz.pl>
2023-02-16 14:02:27 +0100

    Make nix use dev-packages

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ pkgs.mkShell {
   shellHook = ''
     export PIPENV_VENV_IN_PROJECT=1
     export PIPENV_VERBOSITY=-1
-    ${pkgs.pipenv}/bin/pipenv install --ignore-pipfile
+    ${pkgs.pipenv}/bin/pipenv install --dev --ignore-pipfile
     source .venv/bin/activate
   '';
 }
