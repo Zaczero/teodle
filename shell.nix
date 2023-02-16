0292e156d673dc7ff01f37f0dee8c37b03f65fcc
Kamil Monicz <kamil@monicz.pl>
2023-02-16 10:16:40 +0100

    Support nix-shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python311
+    pipenv
+  ];
+
+  shellHook = ''
+    export PIPENV_VENV_IN_PROJECT=1
+    ${pkgs.pipenv}/bin/pipenv install --ignore-pipfile
+    source .venv/bin/activate
+  '';
+}
